b2bfeb208bf1f738b01a307e47d2b3ec28cd4fc3
Kenny Baskett <kbaskett248@gmail.com>
2021-02-15 15:15:08 -0500

    Button on page to feed the fish

--- fish_feeder/web_app.py
+++ fish_feeder/web_app.py
@@ -1,15 +1,31 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
+
+import api
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates/")
 
 
 @app.get("/")
-def read_root(request: Request):
-    return templates.TemplateResponse("status.html", context={"request": request})
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        "status.html", context={"request": request, "feed_url": "/feed"}
+    )
+
+
+@app.get("/feed")
+async def feed_fish_redirect(request: Request):
+    api.feed_fish()
+    return RedirectResponse("/")
+
+
+@app.post("/api/feed")
+async def feed_fish():
+    api.feed_fish()
 
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run("web_app:app", reload=True)
