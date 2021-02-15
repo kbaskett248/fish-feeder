14288122e470440b682530916f0c21a331c5f40d
Kenny Baskett <kbaskett248@gmail.com>
2021-02-15 16:04:13 -0500

    Common api code

--- fish_feeder/api.py
+++ fish_feeder/api.py
@@ -1,16 +1,19 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class API(ABC):
+    @abstractmethod
     def feed_fish(self):
-        pass
+        print("I did the common stuff")
 
 
 class SimulatedAPI(API):
     def feed_fish(self):
+        super().feed_fish()
         print("I simulated feeding the fish")
 
 
 class DeviceAPI(API):
     def feed_fish(self):
+        super().feed_fish()
         print("I fed the fish")
