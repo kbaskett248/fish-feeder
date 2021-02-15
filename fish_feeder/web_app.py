e0d1501702d645812ffd352cd6e455b0f1526c93
Kenny Baskett <kbaskett248@gmail.com>
2021-02-15 13:39:52 -0500

    status web page

--- fish_feeder/web_app.py
+++ fish_feeder/web_app.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
 
 
 @app.get("/")
-def read_root():
-    return "hello world"
+def read_root(request: Request):
+    return templates.TemplateResponse("status.html", context={"request": request})
 
 
 if __name__ == "__main__":
