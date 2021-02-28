895ac414f9047ccaf7cc6bd1ca3809b6d2b48eb6
Kenneth Baskett <kbaskett248@gmail.com>
2021-02-27 21:49:46 -0500

    Add dev option to fish-feeder command

--- fish_feeder/__init__.py
+++ fish_feeder/__init__.py
@@ -1,7 +1,25 @@
 __version__ = "0.1.0"
 
+import typer
 
-def run():
+
+def run_server(
+    dev: bool = typer.Option(False, help="Use --dev to run in development mode")
+):
+    """
+    Run the server for the Fish Feeder.
+
+    The --dev option will automatically reload when changes are detected.
+
+    """
     import uvicorn
 
-    uvicorn.run("fish_feeder.web_app:app", reload=True)
+    if dev:
+        uvicorn.run("fish_feeder.web_app:app", reload=True)
+    else:
+        uvicorn.run("fish_feeder.web_app:app", host="0.0.0.0")
+
+
+def run():
+
+    typer.run(run_server)
