3442a086151685d83f581109842005fd54b4013c
Kenny Baskett <kbaskett248@gmail.com>
2021-03-10 16:38:09 -0500

    Print package version when application starts

--- fish_feeder/web_app.py
+++ fish_feeder/web_app.py
@@ -13,6 +13,7 @@ from pydantic import BaseModel
 from . import api as api_
 from . import database, scheduler
 from .settings import Settings, get_settings
+from . import __version__ as PACKAGE_VERSION
 
 API_VERSION = "0.1.0"
 
@@ -23,6 +24,8 @@ TEMPLATE_PATH = BASEDIR / "templates"
 app = FastAPI(title="Fish Feeder 5000", version=API_VERSION)
 app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
 templates = Jinja2Templates(directory=TEMPLATE_PATH)
+
+logger.info("Package version: {}", PACKAGE_VERSION)
 
 
 def get_api(settings: Settings = Depends(get_settings)) -> api_.API:
