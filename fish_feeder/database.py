26f405d178e5ab386df3b5bb5900e98454595279
Kenneth Baskett <kbaskett248@gmail.com>
2021-03-28 22:37:45 -0400

    Documentation for the database file

--- fish_feeder/database.py
+++ fish_feeder/database.py
@@ -1,3 +1,5 @@
+# Defines the Database using sqlalchemy.
+
 from datetime import datetime as dt, time
 from datetime import timedelta
 from functools import lru_cache
@@ -15,6 +17,14 @@ from . import abstract
 
 @lru_cache()
 def get_engine(db_url: str):
+    """Return an sqlalchemy engine for the given database url.
+
+    Args:
+        db_url (str): The path to the database
+
+    Returns:
+        [type]: Sqlalchemy database url
+    """
     if db_url.startswith("sqlite"):
         connect_args = {"check_same_thread": False}
     else:
@@ -26,6 +36,15 @@ Base = declarative_base()
 
 
 class Feeding(abstract.Feeding, Base):
+    """Defines a database table for feeding log entries.
+
+    Attributes:
+        id (int): Primary key column
+        time_requested (datetime): Datetime the feed was requested
+        time_fed (datetime): Datetime the feed was performed
+
+    """
+
     __tablename__ = "feeding"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -34,6 +53,16 @@ class Feeding(abstract.Feeding, Base):
 
 
 class Settings(abstract.Settings, Base):
+    """Defines a database table for application settings.
+
+    This table is intended to operate as a singleton.
+
+    Attributes:
+        id (int): Primary key column
+        feed_angle (float): Angle to rotate the motor for a feeding
+
+    """
+
     __tablename__ = "settings"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -41,6 +70,15 @@ class Settings(abstract.Settings, Base):
 
 
 class Schedule(abstract.Schedule, Base):
+    """Defines a database table for feeding schedules.
+
+    Attributes:
+        id (int): Primary key column
+        schedule_type (abstract.ScheduleMode): Type of schedule
+        time_ (time): Time of day for daily schedule
+
+    """
+
     __tablename__ = "schedule"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -50,18 +88,48 @@ class Schedule(abstract.Schedule, Base):
     def __init__(
         self, schedule_type: abstract.ScheduleMode, time_: Optional[time]
     ) -> None:
+        """Initialize the Schedule.
+
+        Args:
+            schedule_type (abstract.ScheduleMode): Type of schedule
+            time_ (Optional[time]): Time of day for daily schedule
+
+        Raises:
+            Exception: An exception is raised if time_ is missing and
+                schedule_type is DAILY
+        """
         if schedule_type is abstract.ScheduleMode.DAILY and time_ is None:
             raise Exception("Must specify time_ if schedule_type is DAILY")
         super().__init__(schedule_type=schedule_type, time_=time_)
 
     def get_id(self) -> str:
+        """Return a unique id for the Schedule.
+
+        Returns:
+            str: A unique id
+        """
         return str(self.id)
 
 
 class Database(abstract.Database):
+    """A Database implementation using these tables.
+
+    Attributes:
+        session: An sqlalchemy Session instance
+
+    """
+
     session: Session
 
     def __init__(self, session: Session) -> None:
+        """Initialize the Database instance.
+
+        The call to self._get_settings initializes the Settings singleton in
+        case it doesn't already exist.
+
+        Args:
+            session (Session): A Session instance
+        """
         self.session = session
         # Initialize settings
         self._get_settings()
